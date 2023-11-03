a092fd18aed8e2ee83f03d9a29666b40d937cbeb
Khingz <kingsley99m5@gmail.com>
2023-11-03 16:47:25 +0100

    initial base model setup

---
+++ models/base_model.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Contains class BaseModel where future models will be derived
+"""
+
+
+class BaseModel:
+    """BaseModel class from which future classes will be derived"""
+    pass
