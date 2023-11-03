6d794483977430ce99e2910761197fa672b8a65b
Kingsley Akpan <54294050+Khingz@users.noreply.github.com>
2023-11-03 23:35:06 +0100

    implemeted BaseModel, DbStorage and Initial USer Model

--- models/base_model.py
+++ models/base_model.py
@@ -27,6 +27,8 @@ class BaseModel():
                     if key in ['created_at', 'updated_at']:
                         val = datetime.fromisoformat(val)
                     setattr(self, key, val)
+            if 'id' not in kwargs:
+                self.id = str(uuid.uuid4())
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
