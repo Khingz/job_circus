20d1d55359f0fadc07fd5b556a7463ff2e94bde0
Khingz <kingsley99m5@gmail.com>
2023-11-25 20:40:50 +0100

    feat: 50% complete single page

--- web_flask/forms/post_job.py
+++ web_flask/forms/post_job.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
 """Post form extended from flask wtf"""
 from flask_wtf import FlaskForm
-from wtforms import (StringField, DateField, SubmitField)
+from wtforms import (StringField, DateField, SubmitField, TextAreaField, RadioField)
 from wtforms.validators import InputRequired, Length
 
 class PostJobForm(FlaskForm):
     """Pst job form class"""
     title = StringField('Title', validators=[InputRequired(),
                                              Length(min=4, max=30)])
-    description =StringField('Description', validators=[InputRequired(),
-                                             Length(min=10, max=5000)])
+    description = TextAreaField('Description', validators=[InputRequired(),
+                                             Length(min=10, max=2000)],
+                                             render_kw={'style': 'height: 5rem; resize: none;'})
     location = StringField('loation', validators=[InputRequired(),
                                              Length(min=4, max=100)])
     salary = StringField('Salary', validators=[InputRequired(),
                                              Length(min=1, max=15)])
     requirements = StringField('Requirements', validators=[InputRequired(),
                                              Length(min=4, max=1000)])
+    type = RadioField('type', choices=['Partime', 'Fulltime'],
+                       validators=[InputRequired()])
     deadline = DateField('Deadline', format='%Y-%m-%d')
     submit = SubmitField('Post')
