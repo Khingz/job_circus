07b50f4e6046096e3a31577f48945ce0e097f7a2
Kingsley Akpan <54294050+Khingz@users.noreply.github.com>
2023-12-05 19:19:39 +0100

    working on landing page

--- web_flask/forms/post_job.py
+++ web_flask/forms/post_job.py
@@ -7,16 +7,16 @@ from wtforms.validators import InputRequired, Length
 class PostJobForm(FlaskForm):
     """Pst job form class"""
     title = StringField('Title', validators=[InputRequired(),
-                                             Length(min=4, max=30)])
+                                             Length(min=1, max=30)])
     description = TextAreaField('Description', validators=[InputRequired(),
                                              Length(min=10, max=2000)],
                                              render_kw={'style': 'height: 5rem; resize: none;'})
     location = StringField('loation', validators=[InputRequired(),
-                                             Length(min=4, max=100)])
+                                             Length(min=1, max=100)])
     salary = StringField('Salary', validators=[InputRequired(),
                                              Length(min=1, max=15)])
     requirements = StringField('Requirements', validators=[InputRequired(),
-                                             Length(min=4, max=1000)])
+                                             Length(min=1, max=1000)])
     type = RadioField('type', choices=['Partime', 'Fulltime'],
                        validators=[InputRequired()])
     deadline = DateField('Deadline', format='%Y-%m-%d')
